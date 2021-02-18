84af466571a13654da4bf3594702509660309033
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-02-18 10:06:49 +0000

    remove more redundancy

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 export const Footer = props => (
 	<footer className="footer mt-auto py-3 container-fluid">
 		<div className="row d-flex justify-content-center">
-			<div className="col-2 text-white">Project Hunger</div>
-			<div className="offset-7 col-2 text-white">
+			<div className="col-2">Project Hunger</div>
+			<div className="offset-7 col-2">
 				{props.footer_text}
 				{"  "}
 				<a href={props.footer_url}>Click aquí</a>
