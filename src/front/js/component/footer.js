5ef34631f62be32c724616cb210a8985cb67d952
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-02-18 08:52:44 +0000

    some paddings

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 
 export const Footer = props => (
 	<footer className="footer mt-auto py-3 footer--background-color container-fluid">
-		<div className="row d-flex justify-content-space-between">
+		<div className="row d-flex justify-content-center">
 			<div className="col-2 text-white">Project Hunger</div>
 			<div className="offset-7 col-2 text-white">
 				{props.footer_text}
-				<a className="p-2 footer--link-color" href={props.footer_url}>
+				<a className="p-3 footer--link-color" href={props.footer_url}>
 					Click aquí
 				</a>
 			</div>
