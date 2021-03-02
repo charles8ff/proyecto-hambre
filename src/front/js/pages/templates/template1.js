8bb7e594b0437b7a118585222a83071d04327d7c
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-03-02 08:56:10 +0000

    template 1 finished

--- src/front/js/pages/templates/template1.js
+++ src/front/js/pages/templates/template1.js
@@ -1,25 +1,26 @@
 import React, { useContext, Fragment, useEffect } from "react";
 import { Context } from "../../store/appContext";
 import "../../../styles/template1.scss";
-import arrow from "../../../styles/img/flecha.png";
+// import arrow from "../../../styles/img/yellowarrow.png";
 
 export const Template1 = () => {
 	// const { store, actions } = useContext(Context);
-	let arrow = require("../../../styles/img/flecha.png");
+	let arrow = require("../../../styles/img/yellowarrow.png");
 
 	const renderFood = () => {
 		var indents = [];
 		for (var i = 0; i < 3; i++) {
-			indents.push(<li key={i}>Es un ejem....... 5 €</li>);
+			indents.push(<li key={i}>Es un ejemplo.......5 €</li>);
 		}
 		return indents;
-	};
+	}; //relleno
 
 	return (
 		<>
-			<div className="container template1-container justify-content-center">
+			<div className="container-fluid template1-container justify-content-center">
 				<h2>MENÚ</h2>
 				<span>BAR MANOLO</span>
+				{/* ^future props */}
 				<div className="row  justify-content-center align-content-center">
 					<h3>
 						<img src={arrow} /> ENSALADAS <img className="flip-horizontally" src={arrow} />
@@ -42,7 +43,11 @@ export const Template1 = () => {
 					</h3>
 				</div>
 				<div className="row justify-content-center align-content-center">
-					<ul>{renderFood()}</ul>
+					<ul>
+						{renderFood()}
+						{renderFood()}
+						{renderFood()}
+					</ul>
 				</div>
 			</div>
 		</>
