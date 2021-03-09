874435c5b7383454d6fcdff34a683437b86f3d6c
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-03-09 15:52:02 +0000

    moving template

--- src/front/js/component/menusview.jsx
+++ src/front/js/component/menusview.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from "react";
+import React, { createRef, Component, useContext, useRef } from "react";
 import { Context } from "../store/appContext";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
@@ -6,60 +6,30 @@ import PropTypes from "prop-types";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useQRCode } from "react-qrcodes";
+import { useScreenshot } from "use-react-screenshot";
 
 export const MenusView = props => {
 	const { store, actions } = useContext(Context);
 
-	// templatesInHTML = store.business.menus o algo del palo
+	const ref = useRef(null);
+	const [image, takeScreenshot] = useScreenshot();
+	const getImage = () => takeScreenshot(ref.current);
 
-	return (
-		<Container>
-			<CardDeck className="justify-content-center pb-5">
-				<Row xs={1} md={1} lg={3} className="justify-content-center">
-					<Col>
-						<Card className="text-center">
-							<Card.Img
-								variant="top"
-								src="https://dkstudio.mx/wp-content/plugins/tutor/assets/images/placeholder.jpg"
-							/>
-							<Card.Body>
-								<Card.Title>Menú del día</Card.Title>
-							</Card.Body>
-							<Card.Footer>
-								<small className="text-muted">Ver toda la carta</small>
-							</Card.Footer>
-						</Card>
-					</Col>
-					<Col>
-						<Card className="text-center">
-							<Card.Img
-								variant="top"
-								src="https://dkstudio.mx/wp-content/plugins/tutor/assets/images/placeholder.jpg"
-							/>
-							<Card.Body>
-								<Card.Title>Carta</Card.Title>
-							</Card.Body>
-							<Card.Footer>
-								<small className="text-muted">Ver toda la carta</small>
-							</Card.Footer>
-						</Card>
-					</Col>
-					<Col>
-						<Card className="text-center">
-							<Card.Img
-								variant="top"
-								src="https://dkstudio.mx/wp-content/plugins/tutor/assets/images/placeholder.jpg"
-							/>
-							<Card.Body>
-								<Card.Title>Carta de vinos</Card.Title>
-							</Card.Body>
-							<Card.Footer>
-								<small className="text-muted">Ver toda la carta</small>
-							</Card.Footer>
-						</Card>
-					</Col>
-				</Row>
-			</CardDeck>
-		</Container>
-	);
+	// templatesInHTML = store.business.menus o algo del palo
+	const [inputRef] = useQRCode({
+		text: "https://www.youtube.com/watch?v=vLRyJ0dawjM/react-qrcodes",
+		options: {
+			level: "M",
+			margin: 7,
+			scale: 1,
+			width: 200,
+			color: {
+				dark: "#000000",
+				light: "#ED750B"
+			}
+		}
+	});
+
+	return <canvas ref={inputRef} />;
 };
