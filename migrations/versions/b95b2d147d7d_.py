e70da6ef4d70699d28ef0a0025979d9054eaf29a
danseks <danseksfzz@gmail.com>
2021-03-11 16:47:30 +0000

    Update

--- migrations/versions/b95b2d147d7d_.py
+++ migrations/versions/b95b2d147d7d_.py
@@ -1,6 +1,6 @@
 """empty message
 
-Revision ID: 13653ac06402
+Revision ID: b95b2d147d7d
 Revises: 
 Create Date: 2021-03-11 15:47:10.496838
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '13653ac06402'
+revision = 'b95b2d147d7d'
 down_revision = None
 branch_labels = None
 depends_on = None
