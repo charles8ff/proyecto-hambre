1cad90ca95430565d33ac8557ce4a907982af599
danseks <danseksfzz@gmail.com>
2021-03-15 11:34:16 +0000

    Fixed some issues of responsive mode

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -50,21 +50,52 @@ export const Home = () => {
 				</div>
 			</div>
 			<div className="parallax--two--home">
-				<div className="container">
-					<div className="d-flex flex-wrap">
-						<Benefits
-							img={"https://i.imgur.com/EoBQLsD.png"}
-							text={"Clientes y empleados estarán libres de contagios con este tipo de cartas."}
-						/>
-						<Benefits
-							img={"https://i.imgur.com/WoHHNhr.png"}
-							text={"Tus clientes tendrán directamente la carta en sus manos y no tendrán que esperar."}
-						/>
-						<Benefits
-							img={"https://i.imgur.com/ePOcOEc.png"}
-							text={"Clientes y empleados estarán libres de contagios con este tipo de cartas."}
-						/>
-					</div>
+				<div className="Parallax_Two--CardContainer">
+					<ul className="Parallax__TwoCards">
+						<li className="Parallax__TwoCards--Item">
+							<div className="Parallax__TwoCard">
+								<div className="d-flex flex-row pb-4 justify-content-center">
+									<div className="card_image">
+										<img src="https://i.imgur.com/EoBQLsD.png" />
+									</div>
+								</div>
+								<div className="Parallax__TwoCard--Content">
+									<p className="m-t-15 text-center text-muted">
+										Clientes y empleados estarán libres de contagios con este tipo de cartas.
+									</p>
+								</div>
+							</div>
+						</li>
+						<li className="Parallax__TwoCards--Item">
+							<div className="Parallax__TwoCard">
+								<div className="d-flex flex-row pb-4 justify-content-center">
+									<div className="card_image">
+										<img src="https://i.imgur.com/WoHHNhr.png" />
+									</div>
+								</div>
+								<div className="Parallax__TwoCard--Content">
+									<p className="m-t-15 text-center text-muted">
+										Tus clientes tendrán directamente la carta en sus manos y no tendrán que
+										esperar.
+									</p>
+								</div>
+							</div>
+						</li>
+						<li className="Parallax__TwoCards--Item">
+							<div className="Parallax__TwoCard">
+								<div className="d-flex flex-row pb-4 justify-content-center">
+									<div className="card_image">
+										<img src="https://i.imgur.com/ePOcOEc.png" />
+									</div>
+								</div>
+								<div className="Parallax__TwoCard--Content">
+									<p className="m-t-15 text-center text-muted">
+										Clientes y empleados estarán libres de contagios con este tipo de cartas.
+									</p>
+								</div>
+							</div>
+						</li>
+					</ul>
 				</div>
 				<div className="dmenu--start--now">
 					<div className="container p-5">
@@ -85,10 +116,10 @@ export const Home = () => {
 			<div className="dmenu--about--what--doing">
 				<div className="container">
 					<div className="row p-5">
-						<div className="col-12 col-md-7 pr-md-5 text-left align-self-center ">
+						<div className="col-12 col-md-5 pr-md-5 text-left align-self-center ">
 							<img src={"https://i.imgur.com/5BkbIdm.jpg"} className="about--us--image avatar__home" />
 						</div>
-						<div className="col-12 col-md-5 ">
+						<div className="col-12 col-md-6">
 							<h2 className="mb-4 font-weight-bold ">Sobre Nosotros</h2>
 							<p>
 								Somos Oscar Fernández y Carlos Fisac, dos desarrolladores web juniors concienciados con
