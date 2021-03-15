4e5188c30a7b4105ee8e953840ad0eb63a7a16f7
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-03-15 14:15:11 +0000

    edit button

--- src/front/js/component/profile.jsx
+++ src/front/js/component/profile.jsx
@@ -13,7 +13,9 @@ import { useState } from "react";
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
 	const [businessMenus, setBusinessMenus] = useState(false);
+	const [editing, setEditing] = useState(false);
 	const history = useHistory();
+
 	useEffect(
 		() => {
 			actions.getProfile(history.location.pathname);
@@ -65,23 +67,48 @@ export const Profile = () => {
 			<div className="UserAcess Profile__Card__over">
 				<div className="container">
 					<div className="row Profile__Card_FullHeight justify-content-center">
-						{/* <div className="col-12"> */}
 						<div className="Profile__Card mx-auto  col-12">
 							<div className="Profile__CardWrapper">
 								<div className="Profile__CardPlace d-flex flex-row">
 									<div className="aside">
-										<img
-											className="avatar"
-											src="https://66.media.tumblr.com/avatar_faa95867d2b3_128.png"
-										/>
-										<div className="d-flex flex-row">
-											<i className="Profile__Icon fas fa-color fa-lg fa-map-marker-alt" />
-											<h4 className="Profile__H4">{store.placeData.address}</h4>
+										<div className="d-flex flex-row align-items-end">
+											<img
+												className="avatar"
+												src="https://66.media.tumblr.com/avatar_faa95867d2b3_128.png"
+											/>
+
+											<OurButton
+												title={<i className="fas fa-2x fa-edit editIcon" />}
+												click={() => {
+													setEditing(!editing);
+												}}
+												hide={store.loginToken != false ? "" : "d-none"}
+											/>
 										</div>
-										<div className="d-flex flex-row">
-											<i className="Profile__Icon fas fa-color fa-lg fa-phone" />
-											<h4 className="Profile__H4">{store.placeData.phone_number}</h4>
-										</div>
+										{editing ? (
+											<>
+												<div className="d-flex flex-row">
+													<i className="Profile__Icon fas fa-color fa-lg fa-map-marker-alt" />
+													<div>voy a editar address</div>
+												</div>
+												<div className="d-flex flex-row">
+													<i className="Profile__Icon fas fa-color fa-lg fa-phone" />
+													<div>voy a editar phone_number</div>
+												</div>
+											</>
+										) : (
+											<>
+												<div className="d-flex flex-row">
+													<i className="Profile__Icon fas fa-color fa-lg fa-map-marker-alt" />
+													<h4 className="Profile__H4">{store.placeData.address}</h4>
+												</div>
+												<div className="d-flex flex-row">
+													<i className="Profile__Icon fas fa-color fa-lg fa-phone" />
+													<h4 className="Profile__H4">{store.placeData.phone_number}</h4>
+												</div>
+											</>
+										)}
+
 										<div className="mt-auto offset-1">
 											<OurButton
 												title="Cerrar Sesión"
@@ -96,20 +123,36 @@ export const Profile = () => {
 
 									{/* <Container> */}
 									<div className="d-flex flex-column justify-content-center">
-										<div className="d-flex flex-row pt-2 justify-content-center Profile__Card--Content">
-											<h2>{store.placeData.place_name}</h2>
+										<div className="d-flex flex-row pt-2 justify-content-center align-items-center Profile__Card--Content">
+											{editing ? (
+												<>
+													<div>voy a editar place_name</div>
+												</>
+											) : (
+												<h2>{store.placeData.place_name}</h2>
+											)}
 										</div>
-										<div className=" d-flex flex-row justify-content-center Profile__Card--Content">
-											<i className="Profile__Icon--Hour far fa-color fa-clock" />
-											<h4 className="Profile__H4">
-												{"Horario: "}
-												{store.placeData.open_hour}
-												{" - "}
-												{store.placeData.close_hour}
-											</h4>
+										<div className=" d-flex flex-row justify-content-center align-items-center Profile__Card--Content">
+											{editing ? (
+												<>
+													<i className="Profile__Icon--Hour far fa-color fa-clock" />
+													<div>voy a editar input open_hour and close_hour</div>
+												</>
+											) : (
+												<>
+													<i className="Profile__Icon--Hour far fa-color fa-clock" />
+													<h4 className="Profile__H4">
+														{"Horario: "}
+														{store.placeData.open_hour}
+														{" - "}
+														{store.placeData.close_hour}
+													</h4>
+												</>
+											)}
 										</div>
+
 										<div className="d-flex flex-row justify-content-center p-2 Profile__Card--ContentMap">
-											<Maps />
+											{editing ? <></> : <Maps />}
 										</div>
 										{businessMenus ? (
 											<>
@@ -126,7 +169,6 @@ export const Profile = () => {
 														click={() => {
 															history.push(history.location.pathname.concat("/addmenu"));
 														}}
-														// hide={store.loginToken != false ? "" : "d-none"}
 													/>
 												</div>
 											</>
@@ -144,33 +186,19 @@ export const Profile = () => {
 													click={() => {
 														history.push(history.location.pathname.concat("/addmenu"));
 													}}
-													// hide={store.loginToken != false ? "" : "d-none"}
 												/>
 											</div>
 										)}
 										<div className="d-flex flex-row flex-wrap card__profile pl-1">
 											{MenusInHTML()}
 										</div>
-										{/* <p className="p-3">{store.placeData.description}</p>
-										<OurButton
-											title="Editar"
-											click={() => history.push(`${store.placeData.id}/edit`)}
-											hide={
-												store.loginToken != false &&
-												actions.decodeToken(store.loginToken).sub.id == store.placeData.id
-													? ""
-													: "d-none"
-											}
-										/> */}
 									</div>
-									{/* </Container> */}
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			{/* </div> */}
 		</>
 	);
 };
